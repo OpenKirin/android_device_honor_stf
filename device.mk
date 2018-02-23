e9db68443c4f0fa7a2073bb43fd1b651fdeee5cc
Huawei-Dev <surdu_petru@yahoo.com>
2018-02-23 21:07:32 +0100

    stf: disable sdcardfs

--- device.mk
+++ device.mk
@@ -60,6 +60,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION)
 
+# Sdcardfs
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.sys.sdcardfs=0 \
+    persist.sys.sdcardfs.emulated=0 \
+    persist.sys.sdcardfs.public=0
+
 # VNDK
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vndk-compat/ld.config.compat.txt:system/etc/ld.config.compat.txt \
