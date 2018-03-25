1ea4615ab5be1b02423c30b080b8a208b250a89c
jrizzoli <joey@lineageos.org>
2018-03-25 12:21:42 +0200

    stf: build textclassifier

--- device.mk
+++ device.mk
@@ -81,6 +81,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION)
 
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # Sdcardfs
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.sys.sdcardfs=0 \
